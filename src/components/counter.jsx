927fa2856dd34a5dafffb5d34a0c1ece40e96681
Aisylu Khaiatova <belle.femme.de.lune@gmail.com>
2021-08-26 10:17:55 +0300

    first counter

---
+++ src/components/counter.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const tags = ["tag1", "tag2", "tag3"];
+
+  const formCount = () => {
+    return count === 0 ? "Ноль" : count;
+  };
+
+  const getBagecclasses = () => {
+    let classes = "badge m-2 bg-";
+    classes += count === 0 ? "danger" : "primary";
+    return classes;
+  };
+
+  const renderTags = () => {
+    if (tags.length === 0) return "Тегов не найдено";
+    return tags.map((tag) => <li key={tag}>{tag}</li>);
+  };
+
+  const handleIncrement = (productId) => {
+    console.log(productId);
+    setCount(count + 1);
+  };
+
+  const handleDecrement = (productId) => {
+    console.log(productId);
+    if (count > 1) setCount(count - 1);
+    else setCount(0);
+  };
+
+  return (
+    <>
+      <span className={getBagecclasses()}>{formCount()}</span>
+      <button
+        onClick={() => handleIncrement({ id: 1 })}
+        className="btn btn-secondary btn-sm"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => handleDecrement({ id: 1 })}
+        className="btn btn-secondary btn-sm"
+      >
+        Decrement
+      </button>
+    </>
+  );
+};
+
+export default Counter;
